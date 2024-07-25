6883629d8820458e0d8275a15f8d3fb0f26a5900
pockx96 <pock.kriz@gmail.com>
2024-07-25 08:55:37 -0700

    add toast notifications

--- src/app/_components/task-manager.tsx
+++ src/app/_components/task-manager.tsx
@@ -1,20 +1,23 @@
 "use client";
 import { Task } from "@prisma/client";
-import { Input } from "postcss";
-import { useState } from "react";
-
+import { useState,useEffect, useRef } from "react";
 import { api } from "~/trpc/react";
+import toast, { Toaster } from 'react-hot-toast';
+
+
 
 export const TaskManager = () => {
   const [title, setTitle] = useState("");
   const [titleFind, setTitleFind] = useState("");
   const [description, setDescription] = useState("");
   const [done, setDone] = useState(true);
-
   const [titleToDelete, setTitleToDelete] = useState("");
 
   const utils = api.useUtils();
   const tasks = api.task.getAll.useQuery();
+
+  const notifyNewTask = () => toast.success('New Task Add.');
+  const notifyDeleteTask = () => toast.error('Task Delete Succesful')
 
   const handleUpdateTask = api.task.updateTask.useMutation({
     onSuccess: async () => {
@@ -22,9 +25,14 @@ export const TaskManager = () => {
     },
   });
 
-  const taskIsDone = (task: Task)=>{
-    handleUpdateTask.mutate({id:task.id,title:task.title,description:task.description,done:false});
-  }
+  const taskIsDone = (task: Task) => {
+    handleUpdateTask.mutate({
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      done: false,
+    });
+  };
 
   const handleCreateTask = api.task.createTask.useMutation({
     onSuccess: async () => {
@@ -44,11 +52,26 @@ export const TaskManager = () => {
     },
   });
 
-  return (
-    <section className="mx-auto overflow-auto flex h-full w-full scr">
+  const [show, setShow] = useState(false);
+  const toastRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => {
+        setShow(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [show]);
+
+  const launchToast = () => {
+    setShow(true);
+  };
+
+  return (
+    <section className="scr mx-auto flex h-full w-full overflow-auto">
       {/* create task */}
-      <div className="flex w-1/3 justify-center items-center">
+      <div className="flex w-1/3 items-center justify-center">
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -62,41 +85,43 @@ export const TaskManager = () => {
             placeholder="Title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className="text-black"
+            className="bg-white/10 text-black"
           />
           <input
             type="text"
             placeholder="Description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            className="text-black"
+            className="h-1/4 bg-white/10 text-black"
           />
           <button
             type="submit"
-            className="hover:blue-600 bg-blue-500"
+            className="hover:blue-600 h-1/4 bg-blue-500/50 hover:bg-blue-600/85 hover:font-bold"
             disabled={handleCreateTask.isPending}
+            onClick={notifyNewTask}
           >
             {handleCreateTask.isPending ? "Sunmitting" : "Submit"}
           </button>
         </form>
       </div>
-
+      <Toaster />
       {/* get all task unfinished */}
       <div className="flex w-1/3 flex-col items-center pt-4">
         {tasks.data &&
           tasks.data
             .filter((task) => task.done)
             .map((task) => (
-              <div className="my-2 flex justify-center w-1/2 flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
-                <h3 className="text-2xl font-bold text-center">{task.title}</h3>
-                <div className="text-lg text-center">{task.description}</div>
+              <div className="my-2 flex w-1/2 flex-col justify-center gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
+                <h3 className="text-center text-2xl font-bold">{task.title}</h3>
+                <div className="text-center text-lg">{task.description}</div>
                 <button
                   onClick={() => {
                     setTitleToDelete(task.title);
                     handleDeleteTask.mutate({ titleToDelete });
                     tasks.refetch();
+                    notifyDeleteTask();
                   }}
-                  className=" rounded bg-red-600/50 hover:bg-red-600/85 hover:font-bold "
+                  className="rounded bg-red-600/50 hover:bg-red-600/85 hover:font-bold"
                 >
                   Delete
                 </button>
@@ -120,7 +145,9 @@ export const TaskManager = () => {
             .filter((task) => !task.done)
             .map((task) => (
               <div className="my-2 flex w-1/2 flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
-                <h3 className="text-2xl font-bold">{task.title}</h3>
+                <h3 className="text-2xl font-bold line-through">
+                  {task.title}
+                </h3>
                 <div className="text-lg">{task.description}</div>
                 <button
                   onClick={() => {
@@ -128,13 +155,14 @@ export const TaskManager = () => {
                     handleDeleteTask.mutate({ titleToDelete });
                     tasks.refetch();
                   }}
-                  className="rounded bg-red-500"
+                  className="rounded bg-red-600/50 hover:bg-red-600/85"
                 >
                   delete
                 </button>
               </div>
             ))}
       </div>
+
     </section>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,15 @@ import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
-
 import { useState } from "react";
 import { HomeClient } from "./_components/home-client";
 import { TaskManager } from "./_components/task-manager";
+import toast, { Toaster } from 'react-hot-toast';
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
 
   //define functions
+
 
   return (
     <main className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white ">
@@ -20,7 +18,7 @@ export default async function Home() {
         <div className="w-1/3 flex justify-center items-center"><strong>To Do</strong></div>
         <div className="w-1/3 flex justify-center items-center"><strong>Finished</strong></div>
       </nav>  
-      <TaskManager />
+      <TaskManager/>
     </main>
   );
 }
